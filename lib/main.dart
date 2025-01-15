5fbc6b3556eb33899e0e8fca2ab351358aadedeb
Sheiger <emersonquispe2808@gmail.com>
2025-01-15 16:44:36 -0500

    finish project of counter of clicks

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:yes_no_maybe_app/presentation/screens/counter/counter_screen.dart';
+import 'package:yes_no_maybe_app/presentation/screens/counter/counter_functions_screen.dart';
+//import 'package:yes_no_maybe_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreen()
+      home: CounterFunctionScreen()
     );
   }
 }
